9b83f527b5927cea3c81ef6569181a37c0154bda
Katharina Elisabeth Zoz <182355016+KatharinaZoz@users.noreply.github.com>
2025-05-25 07:00:58 +0200

    refractoring for easier testing

--- ceaser.py
+++ ceaser.py
@@ -44,51 +44,53 @@ def handleEncrypt(text: str, offset: int):
             print(OUTPUT_MESSAGE.format(index, encrypt(text, index - 1)))
 
 
-
-while(running):
-    try:
-        choice = int(input("Choose: \n [1] Decrypt \n [2] Encrypt \n [3] Exit \n"))
-    except ValueError:
-        print(INVALID_CHOICE_ERROR)
-        continue
-    
-    if choice == EXIT_INPUT:
-        print("Goodbye ^^")
-        break
-    elif choice < 1 or choice > 3:
-        print(INVALID_CHOICE_ERROR)
-        continue
-
-    filler = "cypher" if choice == 1 else "message"
-    text = input("Please enter the {}: \n".format(filler))
-
-    try:    
-        specs = int(input(" [1] Enter offset \n [2] Show all\n [3] Return \n"))
-    except ValueError:
-        print(INVALID_CHOICE_ERROR)
-        continue
-
-    offset = -1
-
-    if specs == EXIT_INPUT:
-        print("Returning to Main Menu...")
-        continue
-    elif specs < 1 or specs > 3:
-        print(INVALID_CHOICE_ERROR)
-        continue
-    elif specs == 1:
+def menu():
+    while(running):
         try:
-            offset = int(input("Enter offset: \n"))
+            choice = int(input("Choose: \n [1] Decrypt \n [2] Encrypt \n [3] Exit \n"))
         except ValueError:
             print(INVALID_CHOICE_ERROR)
             continue
-        if offset < 0:
-            print("Calculating positive offset equivalent...")
-            offset *= -1
-        if offset > 26:
-            offset %= 26
-    if choice == DECRYPT_INPUT:
-        handleDecrypt(text, offset)
-    elif choice == ENCRYPT_INPUT:
-        handleEncrypt(text, offset)
+    
+        if choice == EXIT_INPUT:
+            print("Goodbye ^^")
+            break
+        elif choice < 1 or choice > 3:
+            print(INVALID_CHOICE_ERROR)
+            continue
 
+        filler = "cypher" if choice == 1 else "message"
+        text = input("Please enter the {}: \n".format(filler))
+
+        try:    
+            specs = int(input(" [1] Enter offset \n [2] Show all\n [3] Return \n"))
+        except ValueError:
+            print(INVALID_CHOICE_ERROR)
+            continue
+
+        offset = -1
+
+        if specs == EXIT_INPUT:
+            print("Returning to Main Menu...")
+            continue
+        elif specs < 1 or specs > 3:
+            print(INVALID_CHOICE_ERROR)
+            continue
+        elif specs == 1:
+            try:
+                offset = int(input("Enter offset: \n"))
+            except ValueError:
+                print(INVALID_CHOICE_ERROR)
+                continue
+            if offset < 0:
+                print("Calculating positive offset equivalent...")
+                offset *= -1
+            if offset > 26:
+             offset %= 26
+
+        if choice == DECRYPT_INPUT:
+            handleDecrypt(text, offset)
+        elif choice == ENCRYPT_INPUT:
+            handleEncrypt(text, offset)
+
+menu()
